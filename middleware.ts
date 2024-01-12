f7beaca58a22805d819f6ccd6d2b866626d30eb7
0xGuvnor <yokeyeong@me.com>
2024-01-12 22:33:36 +0100

    remove auth

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,13 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth({
   callbacks: {
     authorized({ req, token }) {
-      const isLoggedIn = !!token;
-      const isOnDashboard = req.nextUrl.pathname.startsWith("/dashboard");
+      // const isLoggedIn = !!token;
+      // const isOnDashboard = req.nextUrl.pathname.startsWith("/dashboard");
 
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false;
-      }
+      // if (isOnDashboard) {
+      //   if (isLoggedIn) return true;
+      //   return false;
+      // }
 
       return true;
     },
